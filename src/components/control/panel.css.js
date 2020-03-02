4e8fe92d8d91d33d4318100ef92749d3000f25a5
mcli830 <39263631+mcli830@users.noreply.github.com>
2020-03-01 17:02:05 -0800

    allow control panel to stretch height

--- src/components/control/panel.css.js
+++ src/components/control/panel.css.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  height: ${props => props.theme.layout.main.height};
-  max-height: ${props => props.theme.layout.main.height};
   width: 100%;
+  height: 100%;
+  min-height: ${props => props.theme.layout.main.height};
   background-color: ${props => props.theme.background.light};
   box-shadow: ${props => props.theme.shadow.sm()};
 `;
